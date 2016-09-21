04e1a7d0273820ac5a4999fb4a7060a6169cc8e9
Paul O'Connor <jamespauloconnor@gmail.com>
2016-09-21 10:29:56 -0400

    Added store project and JavaScript files

---
+++ jsbasics/0.01_integers.js
@@ -0,0 +1,68 @@
+
+var num = 7;
+//var newNum = 5; // this is camel casing 
+var newer_num = 10; // this is snake casing
+
+//console.log() prints to the console
+
+//operations with console.log
+// console.log(5 + 5);
+//TODO - make a subtraction, multiplication and division console.log()
+// console.log(10 - 5);
+// console.log(7*5);
+// console.log(200 / 40);
+
+//Modulus Operator
+
+// console.log(10 % 5); //returns zero
+// console.log(4 % 3);
+
+//TODO - make a modulus that returns zero and another that returns a remainder
+// console.log(25 % 5); //returns zero
+// console.log(25 % 3); //returns one
+
+//TODO - try to make a variable that is assigned an operation and console log it
+//example: var num = 1 + 1
+// var numOp = 25 + 100;
+// console.log(numOp);
+
+//TODO - try to assign numbers to different variables and perform an 
+//operation to them - example var a = 5; var b = 10; console.log(a * b);
+// var a = 5;
+// var b = 10;
+// console.log(a * b);
+
+
+//TODO - try to assign numbers to two different variables then assign 
+//any operation on these two variables to a new variable
+// var c = a + b
+// console.log(c);
+
+var newNum = 10;
+newNum = newNum + 1;
+console.log(newNum);
+newNum++; //short cut for newNum = newNum + 1;
+console.log(newNum);
+
+newNum = newNum - 1;
+console.log(newNum);
+newNum--;
+console.log(newNum);
+
+newNum = newNum + 10;
+console.log(newNum);
+
+newNum += 10; //short cut for writing newNum = newNum + 10;
+console.log(newNum);
+
+//TODO - take what you know from addition and apply it to subraction
+newNum = newNum - 10;
+console.log(newNum);
+newNum -= 10;
+console.log(newNum);
+
+newNum *= 10;
+console.log(newNum);
+
+newNum /= 10;
+console.log(newNum);
